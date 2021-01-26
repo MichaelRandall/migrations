47a294b67a03b53f6e16d38d7511bc07f5a1b506
Michael Randall <m_ran66@hotmail.com>
2021-01-26 13:45:48 -0800

    add routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import { Provider } from 'react-redux'
 import store from './redux/store'
+
+import Footer from '../src/components/shared/Footer'
+import Navbar from '../src/components/shared/Navbar'
+import PageNotFound from '../src/components/shared/PageNotFound'
+
+import StatusPage from '../src/components/pages/views/Status'
+import AboutPage from '../src/components/pages/views/About'
+
 import CakeContainer from './components/CakeContainer'
 import IceCreamContainer from "./components/IceCreamContainer"
 import PieContainer from "./components/PieContainer"
@@ -32,10 +41,24 @@ function App() {
   return (
     <Provider store = {store}>
       <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={CakeContainer} />
+          <Route path="/icecream" component={IceCreamContainer} />
+          <Route path="/pie" component={PieContainer} />
+          <Route path="/about" component={AboutPage} />
+          {/* <Route path="/members" component={MembersPage} /> */}
+          {/* <Route path="/migrations" component={MigrationsPage} /> */}
+          {/* <Route path="/migration/:name" component={ManageMigrationPage} /> */}
+          {/* <Route path="/migration" component={ManageMigrationPage} /> */}
+          <Route path="/status" component={StatusPage} />
+          <Route component={PageNotFound} />
+        </Switch>
         <CakeContainer />
         <IceCreamContainer />
         <PieContainer />
       </div>
+      <Footer />
     </Provider>
   );
 }
