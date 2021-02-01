23eed9d0c89827f794dffc5c1727de133508e77d
Michael Randall <m_ran66@hotmail.com>
2021-01-31 19:14:31 -0800

    updated stageActions and actionTypes

--- src/redux/actions/stageActions.js
+++ src/redux/actions/stageActions.js
@@ -1,6 +1,5 @@
 import * as types from "./actionTypes";
 import {
-<<<<<<< HEAD
     getStagesApi,
     updateStagesApi
   } from "../../api/stageApi";
@@ -30,67 +29,3 @@ export function getStagesAction(props) {
       });
     };
   }
-=======
-  createStageApi,
-  deleteStageApi,
-  getStageApi,
-  getStagesApi,
-  updateStageApi,
-} from "../../api/stageApi";
-
-// Where are the props values coming from?
-// What are the details
-export function createStageAction(props) {
-  return (dispatch) => {
-    // createMigrationApi takes props and gets details, it sends details to the reducer with type
-    return createStageApi(props).then((details) => {
-      // Dispatch takes an object with details and a type and updates the store with it
-      dispatch({ details, type: types.CREATE_STAGE_SUCCESS });
-    });
-  };
-}
-
-export function deleteStageAction(details) {
-  return (dispatch) => {
-    return deleteStageApi(details).then(() => {
-      dispatch({ details, type: types.DELETE_STAGE_SUCCESS });
-    });
-  };
-}
-
-// Gets a filtered or single migration
-export function getStageAction(props) {
-  return (dispatch) => {
-    return getStageApi(props)
-      .then((details) => {
-        dispatch({ details, type: types.GET_STAGE_SUCCESS });
-      })
-      .catch((error) => {
-        throw error;
-      });
-  };
-}
-
-// Gets full list of migrations
-export function getStagesAction(query = {}) {
-  return (dispatch) => {
-    return getStagesApi(query).then((results) => {
-      dispatch({ results, type: types.GET_STAGES_SUCCESS });
-    });
-  };
-}
-
-export function resetStageAction() {
-  return (dispatch) => {
-    dispatch({ type: types.RESET_STAGE_SUCCESS });
-  };
-}
-
-export function updateStageAction(props) {
-  return (dispatch) => {
-    return updateStageApi(props).then((details) => {
-      dispatch({ details, type: types.UPDATE_STAGE_SUCCESS });
-    });
-  };
-}
->>>>>>> dc40e60b314feae765b3bc7fcd8cff6ee6409bb6
