dddd22454c1508356b84ba41665c3c685f3c178f
Michael Randall <m_ran66@hotmail.com>
2021-01-31 19:16:15 -0800

    updated stageApi

--- src/api/stageApi.js
+++ src/api/stageApi.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 //Query | Search Operation - Gets all
 export function getStagesApi(params) {
     return axios.get("/stages", { params });
@@ -13,28 +12,3 @@ export function getStagesApi(params) {
   export function resetStagesApi(data) {
     return axios.post("/stages", { data });
   }
-=======
-//CRUD Operations
-
-export function createStageApi(data) {
-  return axios.put("/stage", { data });
-}
-
-export function deleteStageApi(data) {
-  return axios.delete("/stage", { data });
-}
-
-// gets one filtered
-export function getStageApi(params) {
-  return axios.get("/stage", { params });
-}
-
-export function updateStageApi(data) {
-  return axios.post("/stage", { data });
-}
-
-//Query | Search Operation - Gets all
-export function getStagesApi(params) {
-  return axios.get("/stages", { params });
-}
->>>>>>> dc40e60b314feae765b3bc7fcd8cff6ee6409bb6
